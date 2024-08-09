c71554012aa11448808f2990de40c6143bf60e64
Ilya Korshunov <ilya.korshunov@issuu.com>
2024-08-09 18:42:11 +0200

    Install React Flow & Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
+
+import '@xyflow/react/dist/style.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
